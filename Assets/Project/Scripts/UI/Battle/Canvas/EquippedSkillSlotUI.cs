8729b27f15f67cd44776d96a9fec4174ba429aa1
Erang <erang960913@gmail.com>
2026-06-14 20:09:20 +0900

    전투씬 패널
    스킬패널에 마우스호버시 효과보임

--- Assets/Project/Scripts/UI/Battle/Canvas/EquippedSkillSlotUI.cs
+++ Assets/Project/Scripts/UI/Battle/Canvas/EquippedSkillSlotUI.cs
@@ -1,25 +1,91 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Relic.Gameplay.Data;
 
-public class EquippedSkillSlotUI : MonoBehaviour
+public class EquippedSkillSlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("Icon")]
     [SerializeField] private Image iconImage;
+
+    [Header("Interaction")]
+    [SerializeField] private Button button;
+    [SerializeField] private bool hideEmptySlotIcon = true;
+
+    private EquippedSkillPanelUI ownerPanel;
+    private SkillMasterData skillData;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        if (iconImage == null)
+            iconImage = GetComponent<Image>();
+
+        if (button == null)
+            button = GetComponent<Button>();
+    }
+
+    private void OnDisable()
+    {
+        if (ownerPanel != null)
+            ownerPanel.HideSkillTooltip();
+    }
 
     public void SetSkill(Sprite icon)
     {
-        if (iconImage == null)
-            return;
+        SetSkill(null, null, icon);
+    }
 
-        iconImage.sprite = icon;
-        iconImage.enabled = icon != null;
+    public void SetSkill(EquippedSkillPanelUI owner, SkillMasterData data, Sprite icon)
+    {
+        ownerPanel = owner;
+        skillData = data;
+
+        if (iconImage != null)
+        {
+            iconImage.sprite = icon;
+            iconImage.enabled = icon != null || !hideEmptySlotIcon;
+            iconImage.raycastTarget = true;
+        }
+
+        if (button != null)
+            button.interactable = skillData != null;
     }
 
     public void Clear()
     {
-        if (iconImage == null)
+        ownerPanel = null;
+        skillData = null;
+
+        if (iconImage != null)
+        {
+            iconImage.sprite = null;
+            iconImage.enabled = !hideEmptySlotIcon;
+            iconImage.raycastTarget = true;
+        }
+
+        if (button != null)
+            button.interactable = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (ownerPanel == null || skillData == null)
             return;
 
-        iconImage.sprite = null;
-        iconImage.enabled = false;
+        if (rectTransform == null)
+            rectTransform = GetComponent<RectTransform>();
+
+        ownerPanel.ShowSkillTooltip(skillData, rectTransform);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (ownerPanel == null)
+            return;
+
+        ownerPanel.HideSkillTooltip();
     }
 }
