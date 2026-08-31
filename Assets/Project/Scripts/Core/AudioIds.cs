1a121ceb416113749767e0becde6e9b368bff2cf
BIRANG22 <yundongmin9@gmail.com>
2026-08-31 20:03:01 +0900

    EventSfx 클래스 추가

--- Assets/Project/Scripts/Core/AudioIds.cs
+++ Assets/Project/Scripts/Core/AudioIds.cs
@@ -44,4 +44,10 @@ public static class AudioIds
         public const string BattleBossRevealImpactLow = "battle.boss.reveal.impact.low";
         public const string BattleBossRevealImpactHigh = "battle.boss.reveal.impact.high";
     }
+
+    public static class EventSfx
+    {
+        public const string Event09Click = "event..09.Skylight";
+    }
+
 }
